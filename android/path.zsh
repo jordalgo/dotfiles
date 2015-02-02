eef9846fefeac50fbd67fef7486318dbfecf9b44
Jordalgo <jordalgo@spotify.com>
2015-02-02 12:11:43 -0500

    updated android ndk

--- android/path.zsh
+++ android/path.zsh
@@ -1,5 +1,5 @@
 ANDROID_HOME=/Users/jordalgo/Android/sdk
-ANDROID_NDK=/Users/jordalgo/Android/android-ndk-r9d
+ANDROID_NDK=/Users/jordalgo/Android/android-ndk-r10d
 export ANDROID_HOME
 export ANDROID_NDK
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home
