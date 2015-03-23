f919c656256f3c8a6cd5db8c1c9973131c8f1646
Jordalgo <jordalgo@spotify.com>
2015-03-23 13:00:05 -0400

    SAVEPOINT

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,4 +1,8 @@
 alias gitsuir="git submodule update --init --recursive"
 alias gpsu='git push --set-upstream origin $(current_branch)'
 alias gacane="git add -A && git commit --amend --no-edit"
+alias gec="git config --global -e"
+alias gup="git pull --rebase --prune && gitsuir"
+alias gsave="git add -A && git commit -m 'SAVEPOINT' "
+
 
