da0ccd5f94bceda7e91ee6a425a74167adcac748
Jordalgo <jordalgo@spotify.com>
2015-03-31 13:49:56 -0400

    added git and rmtrash aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -4,5 +4,7 @@ alias gacane="git add -A && git commit --amend --no-edit"
 alias gec="git config --global -e"
 alias gup="git pull --rebase --prune && gitsuir"
 alias gsave="git add -A :/ && git commit -m 'SAVEPOINT' "
+alias gundo="git reset HEAD~1 --mixed"
+alias gbclean="git branch --merged master | grep -v '\* master' | xargs -n 1 git branch -d"
 
 
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,4 +2,5 @@ alias reload='. ~/.zshrc'
 alias pmd="~/bin/pmd/bin/run.sh"
 alias c="clear"
 alias vim='/Applications/MacVim.app/Contents/MacOS/Vim'
+alias del="rmtrash"
 
