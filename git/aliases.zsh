f7a6531ca8139fc4a640c22d2db497b9add15151
Jordalgo <jordalgo@spotify.com>
2015-09-30 13:09:53 -0400

    Add eslintrc and another git alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,6 +2,7 @@ alias gitsuir="git submodule update --init --recursive"
 alias gpsu='git push --set-upstream origin $(current_branch)'
 alias gacane="git add -A && git commit --amend --no-edit"
 alias gec="git config --global -e"
+alias gac="git add -A && git commit"
 alias gup="git pull --rebase --prune && gitsuir"
 alias gsave="git add -A :/ && git commit -m 'SAVEPOINT' "
 alias gundo="git reset HEAD~1 --mixed"
