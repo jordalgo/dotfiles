4309cc97c38676de112bb1a01e915275e352ec97
Jordan Rome <jordalgo@fb.com>
2018-02-22 14:59:26 -0500

    Add: aliases with backups for harsh cmds

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,7 @@ alias pmd="~/bin/pmd/bin/run.sh"
 alias c="clear"
 alias vim='/usr/local/Cellar/macvim/8.0-145/MacVim.app/Contents/MacOS/Vim'
 alias del="rmtrash"
+alias cp='cp --backup=numbered'
+alias ln='ln --backup=numbered'
+alias mv='mv -f --backup=numbered'
 
